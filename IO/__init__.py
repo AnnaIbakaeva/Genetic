af4ecb17bd08d4fbc3ec48ec54c35d2ec79710b2
Mirann <annaibakaeva@google.com>
2015-04-13 18:44:22 +0600

    + some reproduction

--- IO/__init__.py
+++ IO/__init__.py
@@ -3,13 +3,32 @@ __author__ = 'Анна'
 from tree_creation import TreeCreator
 from functions import Function
 
+
+def create_grow_tree_population(count, creator):
+    population = []
+    for i in count:
+        creator.create(False)
+        population.append(tree_creator.tree)
+    return population
+
+
+def create_full_tree_population(count, creator):
+    population = []
+    for i in count:
+        creator.create(True)
+        population.append(tree_creator.tree)
+    return population
+
+
 tree_creator = TreeCreator(5)
-tree_creator.create(False)
+full_trees = create_full_tree_population(10, tree_creator)
+grow_trees = create_grow_tree_population(10, tree_creator)
+init_population = full_trees + grow_trees
 
-for key in tree_creator.tree_map.keys():
-    if isinstance(tree_creator.tree_map[key], Function):
-        print(str(key)+": "+ tree_creator.tree_map[key].function_name)
-    else:
-        print(str(key)+": "+ str(tree_creator.tree_map[key]))
-
-print(tree_creator.tree)
+# for key in tree_creator.tree_map.keys():
+#     if isinstance(tree_creator.tree_map[key], Function):
+#         print(str(key)+": "+ tree_creator.tree_map[key].function_name)
+#     else:
+#         print(str(key)+": "+ str(tree_creator.tree_map[key]))
+#
+# print(tree_creator.tree)
---
+++ IO/reproduction.py
@@ -0,0 +1,22 @@
+
+
+class Reproductor(object):
+    sum_fitness = 0
+    def __init__(self, population):
+        self._init_population = population
+        self._selected_population = []
+        self.get_sum_fitness_function()
+
+    def get_sum_fitness_function(self):
+        for individual in self._init_population:
+            self.sum_fitness += self._get_fitness(individual)
+
+    def _get_fitness(self, individual):
+        return 0
+
+    def select(self):
+        for individual in self._init_population:
+            count = round(self._get_fitness(individual) / self.sum_fitness)
+            while count > 0:
+                self._selected_population.append(individual)
+                count -= 1
